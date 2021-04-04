5661d747b55b26e53adfc08a7cfdb5e4e557e1a2
Bereczki Ferenc <Fbereczki@gmail.com>
2021-04-05 01:58:30 +0200

    Megjelenés véglegesítve, Admin és Shop felület kész. Testreszabási funkció kész.

---
+++ Zoo/Controllers/ShopController.cs
@@ -0,0 +1,30 @@
+﻿using Zoo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zoo.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly zooContext _context;
+
+        public ShopController(zooContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Invoice()
+        {
+            return View();
+        }
+
+        public IActionResult ecommerce()
+        {
+            return View();
+        }
+
+    }
+}
+
